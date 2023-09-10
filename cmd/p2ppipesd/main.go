0f478cd06dfb69dfa79854354ff44bf2991386f6
YI <uuuuuu@protonmail.com>
2023-09-11 01:35:45 +0800

    get ride of bulk volume of command line arguments parsing

--- cmd/p2ppipesd/main.go
+++ cmd/p2ppipesd/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"encoding/json"
-	"flag"
-	"io"
-	"os"
-	"strings"
-	"time"
 
+	"github.com/alexflint/go-arg"
 	"github.com/contrun/go-p2p-pipes/pkg/daemon"
 	logging "github.com/ipfs/go-log"
 	"github.com/libp2p/go-libp2p"
@@ -18,225 +12,136 @@ import (
 
 var log = logging.Logger("p2ppipesd")
 
-func main() {
-	loglevel := os.Getenv("GOLOG_LOG_LEVEL")
-	if loglevel == "" {
-		logging.SetLogLevel("*", "info")
-	}
+type arguments struct {
+	ListeningAddr            string
+	ID                       string
+	Bootstrap                bool `default:"true"`
+	BootstrapPeers           []string
+	EnableDht                bool `default:"false"`
+	DhtClient                bool `default:"false"`
+	DhtServer                bool `default:"false"`
+	NatPortMap               bool `default:"false"`
+	RelayEnabled             bool `default:"true"`
+	RelayActive              bool `default:"false"`
+	RelayHop                 bool `default:"false"`
+	RelayHopLimit            int  `default:"0"`
+	RelayDiscovery           bool `default:"false"`
+	AutoRelay                bool `default:"false"`
+	AutoNat                  bool `default:"false"`
+	HostAddrs                []string
+	AnnounceAddrs            []string
+	MetricsAddr              string
+	Pprof                    bool
+	PprofPort                int  `default:"0"`
+	UseNoise                 bool `default:"true"`
+	UseTLS                   bool `default:"true"`
+	ForceReachabilityPublic  bool `default:"false"`
+	ForceReachabilityPrivate bool `default:"false"`
+	Muxer                    string
+}
 
-	maddrString := flag.String("listen", "/unix/tmp/p2pd.sock", "daemon control listen multiaddr")
-	id := flag.String("id", "", "peer identity; private key file")
-	bootstrap := flag.Bool("b", false, "connects to bootstrap peers and bootstraps the dht if enabled")
-	bootstrapPeers := flag.String("bootstrapPeers", "", "comma separated list of bootstrap peers; defaults to the IPFS DHT peers")
-	enableDht := flag.Bool("dht", false, "Enables the DHT in full node mode")
-	dhtClient := flag.Bool("dhtClient", false, "Enables the DHT in client mode")
-	dhtServer := flag.Bool("dhtServer", false, "Enables the DHT in server mode (use 'dht' unless you actually need this)")
-	connMgr := flag.Bool("connManager", false, "Enables the Connection Manager")
-	connMgrLo := flag.Int("connLo", 256, "Connection Manager Low Water mark")
-	connMgrHi := flag.Int("connHi", 512, "Connection Manager High Water mark")
-	connMgrGrace := flag.Duration("connGrace", 120*time.Second, "Connection Manager grace period (in seconds)")
-	natPortMap := flag.Bool("natPortMap", false, "Enables NAT port mapping")
-	pubsub := flag.Bool("pubsub", false, "Enables pubsub")
-	pubsubRouter := flag.String("pubsubRouter", "gossipsub", "Specifies the pubsub router implementation")
-	pubsubSign := flag.Bool("pubsubSign", true, "Enables pubsub message signing")
-	pubsubSignStrict := flag.Bool("pubsubSignStrict", true, "Enables or disables pubsub strict signature verification")
-	gossipsubHeartbeatInterval := flag.Duration("gossipsubHeartbeatInterval", 0, "Specifies the gossipsub heartbeat interval")
-	gossipsubHeartbeatInitialDelay := flag.Duration("gossipsubHeartbeatInitialDelay", 0, "Specifies the gossipsub initial heartbeat delay")
-	relayEnabled := flag.Bool("relay", true, "Enables circuit relay")
-	relayActive := flag.Bool("relayActive", false, "Enables active mode for relay")
-	relayHop := flag.Bool("relayHop", false, "Enables hop for relay")
-	relayHopLimit := flag.Int("relayHopLimit", 0, "Sets the hop limit for hop relays")
-	relayDiscovery := flag.Bool("relayDiscovery", false, "Enables passive discovery for relay")
-	autoRelay := flag.Bool("autoRelay", false, "Enables autorelay")
-	autonat := flag.Bool("autonat", false, "Enables the AutoNAT service")
-	hostAddrs := flag.String("hostAddrs", "", "comma separated list of multiaddrs the host should listen on")
-	announceAddrs := flag.String("announceAddrs", "", "comma separated list of multiaddrs the host should announce to the network")
-	noListen := flag.Bool("noListenAddrs", false, "sets the host to listen on no addresses")
-	metricsAddr := flag.String("metricsAddr", "", "an address to bind the metrics handler to")
-	configFilename := flag.String("f", "", "a file from which to read a json representation of the deamon config")
-	configStdin := flag.Bool("i", false, "have the daemon read the json config from stdin")
-	pprof := flag.Bool("pprof", false, "Enables the HTTP pprof handler, listening on the first port "+
-		"available in the range [6060-7800], or on the user-provided port via -pprofPort")
-	pprofPort := flag.Uint("pprofPort", 0, "Binds the HTTP pprof handler to a specific port; "+
-		"has no effect unless the pprof option is enabled")
-	useNoise := flag.Bool("noise", true, "Enables Noise channel security protocol")
-	useTls := flag.Bool("tls", true, "Enables TLS1.3 channel security protocol")
-	forceReachabilityPublic := flag.Bool("forceReachabilityPublic", false, "Set up ForceReachability as public for autonat")
-	forceReachabilityPrivate := flag.Bool("forceReachabilityPrivate", false, "Set up ForceReachability as private for autonat")
-	muxer := flag.String("muxer", "yamux", "muxer to use for connections")
-
-	flag.Parse()
-
-	var c daemon.Config
-
-	if *configStdin {
-		stdin := bufio.NewReader(os.Stdin)
-		body, err := io.ReadAll(stdin)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := json.Unmarshal(body, &c); err != nil {
-			log.Fatal(err)
-		}
-	} else if *configFilename != "" {
-		body, err := os.ReadFile(*configFilename)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := json.Unmarshal(body, &c); err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		c = daemon.NewDefaultConfig()
-	}
-
-	maddr, err := multiaddr.NewMultiaddr(*maddrString)
+func MustGetMultiaddr(s string) multiaddr.Multiaddr {
+	maddr, err := multiaddr.NewMultiaddr(s)
 	if err != nil {
 		log.Fatal(err)
 	}
-	c.ListenAddr = daemon.JSONMaddr{Multiaddr: maddr}
+	return maddr
+}
 
-	if *id != "" {
-		c.ID = *id
-	}
-
-	if *hostAddrs != "" {
-		addrStrings := strings.Split(*hostAddrs, ",")
-		ha := make([]multiaddr.Multiaddr, len(addrStrings))
-		for i, s := range addrStrings {
-			ma, err := multiaddr.NewMultiaddr(s)
-			if err != nil {
-				log.Fatal(err)
-			}
-			(ha)[i] = ma
+func MustGetMultiaddrs(ss []string) (maddrs []multiaddr.Multiaddr) {
+	for _, s := range ss {
+		maddr, err := multiaddr.NewMultiaddr(s)
+		if err != nil {
+			log.Fatal(err)
 		}
-		c.HostAddresses = ha
+		maddrs = append(maddrs, maddr)
+	}
+	return
+}
+
+func getConfig(a *arguments) daemon.Config {
+	c := daemon.NewDefaultConfig()
+
+	if a.ListeningAddr != "" {
+		c.ListenAddr = daemon.JSONMaddr{Multiaddr: MustGetMultiaddr(a.ListeningAddr)}
+	}
+	c.Bootstrap.Peers = MustGetMultiaddrs(a.BootstrapPeers)
+
+	if a.ID != "" {
+		c.ID = a.ID
 	}
 
-	if *announceAddrs != "" {
-		addrStrings := strings.Split(*announceAddrs, ",")
-		ha := make([]multiaddr.Multiaddr, len(addrStrings))
-		for i, s := range addrStrings {
-			ma, err := multiaddr.NewMultiaddr(s)
-			if err != nil {
-				log.Fatal(err)
-			}
-			(ha)[i] = ma
-		}
-		c.AnnounceAddresses = ha
+	if len(a.HostAddrs) != 0 {
+		c.HostAddresses = MustGetMultiaddrs(a.HostAddrs)
 	}
 
-	if *connMgr {
-		c.ConnectionManager.Enabled = true
-		c.ConnectionManager.GracePeriod = *connMgrGrace
-		c.ConnectionManager.HighWaterMark = *connMgrHi
-		c.ConnectionManager.LowWaterMark = *connMgrLo
+	if len(a.AnnounceAddrs) != 0 {
+		c.AnnounceAddresses = MustGetMultiaddrs(a.AnnounceAddrs)
 	}
 
-	if *natPortMap {
-		c.NatPortMap = true
+	c.NatPortMap = a.NatPortMap
+
+	c.Relay.Enabled = a.RelayEnabled
+	c.Relay.Active = a.RelayActive
+	c.Relay.Hop = a.RelayHop
+	c.Relay.Discovery = a.RelayDiscovery
+	c.Relay.HopLimit = a.RelayHopLimit
+
+	c.Relay.Auto = a.AutoRelay
+	c.AutoNat = a.AutoNat
+
+	if len(a.BootstrapPeers) != 0 {
+		c.Bootstrap.Peers = MustGetMultiaddrs(a.BootstrapPeers)
+	}
+	c.Bootstrap.Enabled = a.Bootstrap
+
+	c.Muxer = a.Muxer
+	if a.MetricsAddr != "" {
+		c.MetricsAddress = a.MetricsAddr
 	}
 
-	if *relayEnabled {
-		c.Relay.Enabled = true
-		if *relayActive {
-			c.Relay.Active = true
-		}
-		if *relayHop {
-			c.Relay.Hop = true
-		}
-		if *relayDiscovery {
-			c.Relay.Discovery = true
-		}
-		if *relayHopLimit > 0 {
-			c.Relay.HopLimit = *relayHopLimit
-		}
-	}
-
-	if *autoRelay {
-		c.Relay.Auto = true
-	}
-
-	if *noListen {
-		c.NoListen = true
-	}
-
-	if *autonat {
-		c.AutoNat = true
-	}
-
-	if *pubsub {
-		c.PubSub.Enabled = true
-		c.PubSub.Router = *pubsubRouter
-		c.PubSub.Sign = *pubsubSign
-		c.PubSub.SignStrict = *pubsubSignStrict
-		if *gossipsubHeartbeatInterval > 0 {
-			c.PubSub.GossipSubHeartbeat.Interval = *gossipsubHeartbeatInterval
-		}
-		if *gossipsubHeartbeatInitialDelay > 0 {
-			c.PubSub.GossipSubHeartbeat.InitialDelay = *gossipsubHeartbeatInitialDelay
-		}
-	}
-
-	if *bootstrapPeers != "" {
-		addrStrings := strings.Split(*bootstrapPeers, ",")
-		bps := make([]multiaddr.Multiaddr, len(addrStrings))
-		for i, s := range addrStrings {
-			ma, err := multiaddr.NewMultiaddr(s)
-			if err != nil {
-				log.Fatal(err)
-			}
-			(bps)[i] = ma
-		}
-		c.Bootstrap.Peers = bps
-	}
-
-	if *bootstrap {
-		c.Bootstrap.Enabled = true
-	}
-
-	c.Muxer = *muxer
-
-	if *metricsAddr != "" {
-		c.MetricsAddress = *metricsAddr
-	}
-
-	if *enableDht {
+	if a.EnableDht {
 		c.DHT.Mode = daemon.DHTFullMode
-	} else if *dhtClient {
+	} else if a.DhtClient {
 		c.DHT.Mode = daemon.DHTClientMode
-	} else if *dhtServer {
+	} else if a.DhtServer {
 		c.DHT.Mode = daemon.DHTServerMode
 	}
-
-	if *pprof {
+	if a.Pprof {
 		c.PProf.Enabled = true
-		if pprofPort != nil {
-			c.PProf.Port = *pprofPort
+		if a.PprofPort != 0 {
+			c.PProf.Port = uint(a.PprofPort)
 		}
 	}
 
-	if useTls != nil {
-		c.Security.TLS = *useTls
-	}
-	if useNoise != nil {
-		c.Security.Noise = *useNoise
-	}
+	c.Security.TLS = a.UseTLS
+	c.Security.Noise = a.UseNoise
+	return c
+}
+
+func run() error {
+	var a arguments
+	arg.MustParse(&a)
+	c := getConfig(&a)
 
 	opts := []libp2p.Option{}
-	if *forceReachabilityPrivate && *forceReachabilityPublic {
+	if a.ForceReachabilityPrivate && a.ForceReachabilityPublic {
 		log.Fatal("forceReachability must be public or private, not both")
-	} else if *forceReachabilityPrivate {
+	} else if a.ForceReachabilityPrivate {
 		opts = append(opts, libp2p.ForceReachabilityPrivate())
-	} else if *forceReachabilityPublic {
+	} else if a.ForceReachabilityPublic {
 		opts = append(opts, libp2p.ForceReachabilityPublic())
 	}
 
 	// start daemon
-	_, err = daemon.NewDaemonFromConfig(context.Background(), c, opts...)
+	_, err := daemon.NewDaemonFromConfig(context.Background(), c, opts...)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	select {}
+}
+
+func main() {
+	run()
 }
--- go.mod
+++ go.mod
@@ -20,6 +20,8 @@ require (
 )
 
 require (
+	github.com/alexflint/go-arg v1.4.3 // indirect
+	github.com/alexflint/go-scalar v1.1.0 // indirect
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
