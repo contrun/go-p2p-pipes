ae27616f13a7fbaafc613edc8d17cc462271ae7d
YI <uuuuuu@protonmail.com>
2023-09-11 22:47:05 +0800

    refactor forward for better readability

--- pkg/daemon/forward.go
+++ pkg/daemon/forward.go
@@ -15,22 +15,26 @@ import (
 	manet "github.com/multiformats/go-multiaddr/net"
 )
 
-var ForwardingProtocolID = protocol.ID("/gop2ppipes/forward/v0.1.0")
+var ForwardingProtocolID = protocol.ID("/gop2ppipes/forward/0.1.0")
 
-type ForwardingRequest struct {
+type SetupForwardingRequest struct {
 	// TODO: we should obtain the peer id from rpc the request connection
 	// not from rpc payload set by the peer
 	ID      string
 	Address string
 }
 
-type ForwardingResponse struct{}
+type SetupForwardingResponse struct{}
 
 type ForwardingService struct {
 	daemon *Daemon
 }
 
-func (d *ForwardingService) Forward(ctx context.Context, request ForwardingRequest, response *ForwardingResponse) error {
+// A RPC request to setup a protocol handler which will forward traffic back and forth between
+// a libp2p stream and a connection the multiaddr given in the request (which will be started on demand)
+// After this RPC has successfully returned, the caller may initiate a new stream
+// to the receiver of this RPC which effectively connects to the given multiaddr.
+func (d *ForwardingService) SetupForwarding(ctx context.Context, request SetupForwardingRequest, response *SetupForwardingResponse) error {
 	log.Info("Received a Forwarding call")
 	peer, err := peer.Decode(request.ID)
 	if err != nil {
@@ -41,7 +45,26 @@ func (d *ForwardingService) Forward(ctx context.Context, request ForwardingReque
 		return err
 	}
 	protocolID := d.daemon.GetForwardingProtocolID(peer, addr)
-	d.daemon.SetStreamHandler(protocolID, d.daemon.ForwardStreamHandler(protocolID, peer, addr))
+	streamHandler := func(s network.Stream) {
+		daemon := d.daemon
+		if !IsForwardingStreamAuthorized(s) {
+			s.Reset()
+			log.Debugf("Resetted unauthorized stream", "id", s.ID())
+			return
+		}
+
+		c, err := manet.Dial(addr)
+		if err != nil {
+			log.Debugw("Error dialing in server", "addr", addr.String(), "error", err)
+			s.Reset()
+			return
+		}
+		defer c.Close()
+		defer daemon.RemoveStreamHandler(protocolID)
+		daemon.doStreamPipe(c, s)
+	}
+
+	d.daemon.SetStreamHandler(protocolID, streamHandler)
 	return nil
 }
 
@@ -55,7 +78,7 @@ func (d *Daemon) GetForwardingProtocolID(peer peer.ID, addr multiaddr.Multiaddr)
 	return protocol.ID(peer.String() + "/" + addr.String())
 }
 
-func IsStreamAuthorized(s network.Stream) bool {
+func IsForwardingStreamAuthorized(s network.Stream) bool {
 	log.Debugw("Check stream authorization", "protocol", s.Protocol(), "local peer", s.Conn().LocalPeer(), "remote peer", s.Conn().RemotePeer())
 	return strings.HasPrefix(string(s.Protocol()), s.Conn().RemotePeer().String()+"/")
 }
@@ -80,36 +103,15 @@ func (d *Daemon) doStreamPipe(c net.Conn, s network.Stream) {
 	wg.Wait()
 }
 
-func (d *Daemon) ForwardStreamHandler(protocol protocol.ID, peer peer.ID, localAddr multiaddr.Multiaddr) network.StreamHandler {
-	f := func(s network.Stream) {
-		if !IsStreamAuthorized(s) {
-			log.Debugf("Reset unauthorized stream", "id", s.ID())
-			s.Reset()
-		}
-
-		c, err := manet.Dial(localAddr)
-		if err != nil {
-			log.Debugw("Error dialing in server", "addr", localAddr.String(), "error", err)
-			s.Reset()
-			return
-		}
-		defer c.Close()
-		defer d.RemoveStreamHandler(protocol)
-		d.doStreamPipe(c, s)
-	}
-
-	return f
-}
-
 func (d *Daemon) RequestForwarding(peer peer.ID, remoteAddr multiaddr.Multiaddr, localAddr multiaddr.Multiaddr) error {
 	// TODO: maybe with proactively clean up resources? or back pressue
 	rpcClient := gorpc.NewClient(d.Host, ForwardingProtocolID)
-	request := ForwardingRequest{
+	request := SetupForwardingRequest{
 		ID:      d.ID().String(),
 		Address: remoteAddr.String(),
 	}
-	var response ForwardingResponse
-	err := rpcClient.Call(peer, "ForwardingService", "Forward", request, &response)
+	var response SetupForwardingResponse
+	err := rpcClient.Call(peer, "ForwardingService", "SetupForwarding", request, &response)
 	if err != nil {
 		return err
 	}
